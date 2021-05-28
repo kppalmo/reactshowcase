3f49ba29cc0153490be410d91fb270cb420af4c9
kppalmo <kepalmo8@gmail.com>
2021-05-28 13:16:39 -0400

    addedBootstrap

--- src/App.js
+++ src/App.js
@@ -3,18 +3,25 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import {ImgTest} from './comp/ImgTest';
 import {TxtTest} from './comp/TxtTest';
+import { JubmoTest } from './comp/JumboTest';
+import 'bootstrap/dist/css/bootstrap.css';
+
+
 
 class App extends React.Component{
   render (){
     return(
       <Container>
-        <row>
+        <row> 
           <h1 class="CenterText ">Test Page</h1>
           <ImgTest/>
         </row>  
         <row>
           <TxtTest/>
-          <h1 class="CenterText">More Example Work</h1>
+          <h2 class="CenterText">More Example Work</h2>
+        </row>
+        <row>
+          <JubmoTest/>
         </row>
       </Container>
     )
--- src/comp/ImgTest.jsx
+++ src/comp/ImgTest.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Image } from 'react-bootstrap';
 import sample from '../img/sample.png';
 
 export const ImgTest = props => {
 
     return(
         <div >
-            <img  class="Resize CenterImg " src={sample} alt="test"/>
-            <h1 class="CenterText">The Above Is My Home</h1>
+            <Image  className="Resize CenterImg " src={sample} rounded/>
+            <h2 class="CenterText">The Above Is My Home</h2>
         </div>
         
 
