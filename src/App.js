bbe00f64450c23e3010d864150200937d87fa166
kppalmo <kepalmo8@gmail.com>
2021-05-28 13:39:39 -0400

    work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import {ImgTest} from './comp/ImgTest';
 import {TxtTest} from './comp/TxtTest';
 import { JubmoTest } from './comp/JumboTest';
 import 'bootstrap/dist/css/bootstrap.css';
+import { Navbar } from 'react-bootstrap';
+import { Image } from 'react-bootstrap';
+import sample from '../src/img/sample.png';
+
 
 
 
@@ -12,9 +15,14 @@ class App extends React.Component{
   render (){
     return(
       <Container>
+        <Navbar className="bg">
+          <h1>test</h1>
+          <h1>Test2</h1>
+        </Navbar>
         <row> 
           <h1 class="CenterText ">Test Page</h1>
-          <ImgTest/>
+          <Image  className="Resize CenterImg " src={sample} rounded/>
+          <h2 class="CenterText">The Above Is My Home</h2>
         </row>  
         <row>
           <TxtTest/>
@@ -22,6 +30,8 @@ class App extends React.Component{
         </row>
         <row>
           <JubmoTest/>
+        </row>
+        <row>
         </row>
       </Container>
     )
