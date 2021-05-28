1f60c99660850feb588a468db83a7c9eb39990af
kppalmo <kepalmo8@gmail.com>
2021-05-28 12:03:52 -0400

    txtAdd

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import './App.css';
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import {ImgTest} from './comp/ImgTest';
+import {TxtTest} from './comp/TxtTest';
 
 class App extends React.Component{
   render (){
     return(
       <Container>
         <row>
+          <h1 class="CenterText ">Test Page</h1>
           <ImgTest/>
         </row>  
+        <row>
+          <TxtTest/>
+        </row>
       </Container>
     )
   }
--- src/comp/ImgTest.jsx
+++ src/comp/ImgTest.jsx
@@ -4,12 +4,12 @@ import sample from '../img/sample.png';
 export const ImgTest = props => {
 
     return(
-        <div>
-            <img  class="Resize CenterImg " style="" src={sample}/>
+        <div class="bg">
+            <img  class="Resize CenterImg " src={sample} alt="test"/>
             <h1 class="CenterText">The Above Is My Home</h1>
         </div>
         
 
-        
+
     );
 };
